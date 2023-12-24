1f0f585d92dad692521d87e047dc0f3fbc01d5c4
takumi_64 <t.seno.23.10.1202@4nonome.com>
2023-12-24 10:24:50 +0900

    leap_year_algorithm first @2023/12/24

--- leap_year_algorithm/leap_year_algorithm.dart
+++ leap_year_algorithm/leap_year_algorithm.dart
@@ -1,3 +1,24 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+isLeapYear(year: 2012); //うるう年
+isLeapYear(year: 2300); //うるう年でない
+isLeapYear(year: 2000); //うるう年
+isLeapYear(year: 2023); //うるう年でない
+}
+
+void isLeapYear({int year=0}){
+  bool l; //うるう年ならtrue,でなければfalse
+  if(year%400==0){
+    l=true;
+  }else if(year%100==0){
+    l=false;
+  }else if(year%4==0){
+    l=true;
+  }else{
+    l=false;
+  }
+  if(l==true){
+    print("$year年はうるう年です。");
+  }else{
+    print("$year年はうるう年ではありません。");
+  }
 }
